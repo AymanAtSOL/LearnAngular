d273d725c4a3d6dbffc153a7dd52f6ddebe61453
Ayman Alsahwa <ayman.alsahwa@sol.sa>
2024-06-23 12:22:57 +0300

    passing arguments as signals inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, Input} from '@angular/core';
+import {Component, computed, input, Input} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
 import { CommonModule } from '@angular/common';
 
@@ -12,13 +12,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string ;
-  @Input({required: true}) name: string | undefined ;
-  
+  // @Input({required: true}) avatar!: string ;
+  // @Input({required: true}) name: string | undefined ;
+  // input signals are READ-ONLY
+  avatar = input<string>('');
+  name = input<string>();
   OnSelectedUser() {
 
   }
-  get imagePath() {
-    return './assets/users/'+this.avatar;
-  }
+  imagePath = computed(() => { // angular will recompute this only if avatar is changed
+    return './assets/users/'+this.avatar();
+  })
+  // get imagePath() {
+  //   return './assets/users/'+this.avatar();
+  // }
 }
