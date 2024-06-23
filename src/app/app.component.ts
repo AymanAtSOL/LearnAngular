96ba4fc9382c12d7f966d83232d708c1d7a2ddf0
Ayman Alsahwa <ayman.alsahwa@sol.sa>
2024-06-23 14:22:44 +0300

    my attempt at the exercise Create Tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent]
+    imports: [CommonModule, HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
     users = DUMMY_USERS;
+    selectedUserName:string|undefined = '';
+    onSelectUser(id: string) {
+        this.selectedUserName = DUMMY_USERS.find(user => user.id==id)?.name
+        console.log("Selected User Id is: "+id);
+    }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input, Input} from '@angular/core';
+import {Component, computed, EventEmitter, input, Input, output, Output} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
 import { CommonModule } from '@angular/common';
 
@@ -12,18 +12,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string ;
   @Input({required: true}) avatar!: string ;
   @Input({required: true}) name: string | undefined ;
+  @Output() selehct = new EventEmitter();
+  select = output<string>(); // does the same thing and is used in the same way, but it is shorter and no decorator
   // input signals are READ-ONLY
   // avatar = input<string>('');
   // name = input<string>();
-  OnSelectedUser() {
-
+  get imagePath() {
+    return './assets/users/'+this.avatar;
+  }
+  OnSelectUser(id: string) {
+    this.select.emit(this.id);
   }
   // imagePath = computed(() => { // angular will recompute this only if avatar is changed
   //   return './assets/users/'+this.avatar;
   // })
-  get imagePath() {
-    return './assets/users/'+this.avatar;
-  }
 }
