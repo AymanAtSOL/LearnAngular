5325b75dc0682e395cf582d684786aa3205ccda0
Ayman Alsahwa <ayman.alsahwa@sol.sa>
2024-06-24 15:25:07 +0300

    using localStorage to store them tasks end of section 2

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -5,7 +5,16 @@ import { type NewTask } from "./task/task.model";
 @Injectable({providedIn: 'root'}) // by adding this decorator this instance will be craeted once (Singleton) therefore all other componets are using same data
 export class TasksService {
   private tasks = dummyTasks
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
 
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
   getUserTasks(userId: string){
     return this.tasks.filter(task => task.userId === userId); // id is userId
   }
@@ -17,8 +26,10 @@ export class TasksService {
       summary: task.summary,
       dueDate: task.date,
     });
+    this.saveTasks();
   }
   removeTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id)
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    this.saveTasks();
   }
 }
