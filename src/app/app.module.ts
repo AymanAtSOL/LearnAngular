970b671e4fd3c53701a17f5e1e04914f2dfa46ad
Ayman Alsahwa <ayman.alsahwa@sol.sa>
2024-06-26 10:44:24 +0300

    created shared modules for more structured codebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
 
 // ============//
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { CardComponent } from "./shared/card/card.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-  declarations:[AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent, CardComponent],
+  declarations:[AppComponent, HeaderComponent, UserComponent],
   bootstrap:[AppComponent],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 
 export class AppModule {}
